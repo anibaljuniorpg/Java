2545176bef15c685395cb2e0ac237dd94af8f016
anibal júnior <juninhoguerrap@gmail.com>
2024-01-29 20:51:11 -0300

    Exercicios java
    Arrumar deois

---
+++ curso-em-video/aula14/src/aula/Aula14.java
@@ -0,0 +1,12 @@
+package aula;
+import java.util.Arrays;
+
+
+
+public class Aula14 {
+    public static void main(String[] args) {
+        String[][] paises = {{"Junior", "M"}, {"Manu", "F"}};
+
+        System.out.println(paises[0][0]);
+    }
+}
